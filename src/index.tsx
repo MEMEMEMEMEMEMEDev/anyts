8bdd9cff939b7e8a4a833083496aedd908f2e4b3
mememememememe <marccelohuenchupan@gmail.com>
2022-06-01 11:07:33 -0400

    a

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
-import App from "./App";
 import React from "react";
-import ReactDOM from "react-dom";
-import { render } from "react-dom";
-window.React = React;
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,41 +1,109 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const ESLintWebpackPlugin = require("eslint-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+// refactor
 
 module.exports = {
-  entry: path.join(__dirname, "src/index.js"),
-  output: {
-    path: path.join(__dirname, "bundle"),
-    filename: "bundle.js",
+  entry: {
+    app: "./src/index.tsx",
   },
-  externals: {
-    react: "react",
+  mode: "development",
+  devtool: "source-map",
+  optimization: {
+    usedExports: true,
+  },
+  output: {
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
   module: {
     rules: [
       {
-        test: /\.?js|jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              ["@babel/preset-react", { runtime: "automatic" }],
-            ],
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+            },
           },
-        },
+        ],
       },
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[hash].[ext]",
+              outputPath: "images/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+              publicPath: "fonts/",
+            },
+          },
+        ],
       },
     ],
   },
   plugins: [
+    new ESLintWebpackPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public/index.html"),
-      filename: "index.html",
-      inject: "body",
+      template: "./public/index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./src/assets",
+          to: "./assets",
+        },
+      ],
+    }),
+    new TerserPlugin(),
+    new CssMinimizerPlugin(),
   ],
 };
